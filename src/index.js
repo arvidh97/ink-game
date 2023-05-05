a3a7ae86967954ff2577b5eb26c7733373456ea2
Arvid Hossain <arvidh97@gmail.com>
2023-05-05 18:05:51 -0400

    added player movabilty with boarders of canvas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,127 @@
-import Example from './scripts/example';
+// import Example from './scripts/example';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello world')
+    // console.log('hello world')
     
-    const main = document.getElementById('main');
-    new Example(main);
+    // const main = document.getElementById('main');
+    // new Example(main);
+    const canvas = document.querySelector("#game-canvas")
+    const ctx = canvas.getContext('2d')
+
+    // canvas.width = window.innerWidth
+    // canvas.height = window.innerHeight
+    canvas.width = 1024
+    canvas.height = 576
+
+    const CONSTANTS = {
+        gravity: 0.4,
+    }
+
+    class Player {
+        constructor(position) {
+            this.position = position
+            this.velocity = {
+                x: 0,
+                y: 1
+            }
+            this.width = 50
+            this.height = 50
+            this.isJumping = false
+        }
+    
+        draw() {
+            ctx.fillStyle = 'red'
+            ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        }
+
+        update() {
+            this.draw();
+
+            // this.position.x += this.velocity.x
+            if (this.position.x + this.velocity.x >= 0 && this.position.x + this.velocity.x + this.width <= canvas.width) {
+                this.position.x += this.velocity.x;
+              }
+            
+
+            this.position.y += this.velocity.y
+            if (this.position.y + this.height + this.velocity.y < canvas.height)
+                {this.velocity.y += CONSTANTS.gravity}
+            else 
+                {this.velocity.y = 0; this.isJumping = false}
+        }
+
+        jump() {
+            if (!this.isJumping) {
+                this.velocity.y = -10
+                this.isJumping = true
+            }
+        }
+    }
+
+    const player = new Player({
+        x: 0,
+        y: 0,
+    })
+
+    const keys = {
+        ArrowRight: {
+            pressed: false,
+        },
+        ArrowLeft: {
+            pressed: false,
+        },
+        ArrowUp: {
+            pressed: false,
+        }
+    }
+
+    function animate() {
+        window.requestAnimationFrame(animate)
+            ctx.fillStyle = 'lime';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            player.update();   
+            
+            player.velocity.x = 0
+            if (keys.ArrowRight.pressed) {
+                player.velocity.x = 3
+            }
+            else if (keys.ArrowLeft.pressed) {
+                player.velocity.x = -3
+            }
+
+            if (keys.ArrowUp.pressed) {
+                player.jump()
+            }
+    }
+
+    animate()
+
+    window.addEventListener('keydown', (e) => {
+        switch (e.key) {
+            case 'ArrowRight':
+                keys.ArrowRight.pressed = true
+                break;
+            case 'ArrowLeft':
+                keys.ArrowLeft.pressed = true
+                break;
+            case 'ArrowUp':
+                keys.ArrowUp.pressed = true
+                break;
+        }
+    })
+
+    window.addEventListener('keyup', (e) => {
+        switch (e.key) {
+            case 'ArrowRight':
+                keys.ArrowRight.pressed = false
+                break;
+            case 'ArrowLeft':
+                keys.ArrowLeft.pressed = false
+                break;
+            case 'ArrowUp':
+                keys.ArrowUp.pressed = false
+                break;
+        }
+    })
+    
 })
---
+++ src/scripts/player.js
@@ -0,0 +1,12 @@
+// class Player {
+//     constructor(canvas) {
+//         this.position = {
+//             x: 0,
+//             y: 0,
+//         }
+//     }
+
+//     draw() {
+
+//     }
+// }
