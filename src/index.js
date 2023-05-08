381c3d7deb2dd8b7fb209953cb58f6b44ed5aa1d
Arvid Hossain <arvidh97@gmail.com>
2023-05-08 11:11:51 -0400

    live link update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import Example from './scripts/example';
+import Player from './scripts/player';
 
 document.addEventListener('DOMContentLoaded', () => {
     // console.log('hello world')
@@ -10,58 +11,65 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // canvas.width = window.innerWidth
     // canvas.height = window.innerHeight
-    canvas.width = 1024
-    canvas.height = 576
+    canvas.width = 950
+    canvas.height = 502
 
-    const CONSTANTS = {
-        gravity: 0.4,
-    }
+    // const CONSTANTS = {
+    //     gravity: 0.4,
+    // }
 
-    class Player {
-        constructor(position) {
-            this.position = position
-            this.velocity = {
-                x: 0,
-                y: 1
-            }
-            this.width = 50
-            this.height = 50
-            this.isJumping = false
-        }
+    // class Player {
+    //     constructor(position) {
+    //         this.position = position
+    //         this.velocity = {
+    //             x: 0,
+    //             y: 1
+    //         }
+    //         this.width = 50
+    //         this.height = 50
+    //         this.isJumping = false
+    //     }
     
-        draw() {
-            ctx.fillStyle = 'red'
-            ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
-        }
+    //     draw() {
+    //         ctx.fillStyle = 'red'
+    //         ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    //     }
 
-        update() {
-            this.draw();
+    //     update() {
+    //         this.draw();
 
-            // this.position.x += this.velocity.x
-            if (this.position.x + this.velocity.x >= 0 && this.position.x + this.velocity.x + this.width <= canvas.width) {
-                this.position.x += this.velocity.x;
-              }
+    //         // this.position.x += this.velocity.x
+    //         if (this.position.x + this.velocity.x >= 0 && this.position.x + this.velocity.x + this.width <= canvas.width) {
+    //             this.position.x += this.velocity.x;
+    //           }
             
 
-            this.position.y += this.velocity.y
-            if (this.position.y + this.height + this.velocity.y < canvas.height)
-                {this.velocity.y += CONSTANTS.gravity}
-            else 
-                {this.velocity.y = 0; this.isJumping = false}
-        }
+    //         this.position.y += this.velocity.y
+    //         if (this.position.y + this.height + this.velocity.y < canvas.height)
+    //             {this.velocity.y += CONSTANTS.gravity}
+    //         else 
+    //             {this.velocity.y = 0; this.isJumping = false}
+    //     }
 
-        jump() {
-            if (!this.isJumping) {
-                this.velocity.y = -10
-                this.isJumping = true
-            }
-        }
-    }
+    //     jump() {
+    //         if (!this.isJumping) {
+    //             this.velocity.y = -10
+    //             this.isJumping = true
+    //         }
+    //     }
+    // }
 
     const player = new Player({
         x: 0,
         y: 0,
-    })
+    }, canvas, ctx)
+
+
+    // class Platform {
+    //     constructor() {
+
+    //     }
+    // }
 
     const keys = {
         ArrowRight: {
@@ -79,7 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
         window.requestAnimationFrame(animate)
             ctx.fillStyle = 'lime';
             ctx.fillRect(0, 0, canvas.width, canvas.height);
-            player.update();   
+            player.update();  
             
             player.velocity.x = 0
             if (keys.ArrowRight.pressed) {
--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,12 +1,49 @@
-// class Player {
-//     constructor(canvas) {
-//         this.position = {
-//             x: 0,
-//             y: 0,
-//         }
-//     }
+const CONSTANTS = {
+    gravity: 0.4,
+}
 
-//     draw() {
+class Player {
+    
+    constructor(position, canvas, ctx) {
+        this.position = position
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.width = 50
+        this.height = 50
+        this.isJumping = false
+        this.canvas = canvas
+        this.ctx = ctx
+    }
 
-//     }
-// }
+    draw() {
+        this.ctx.fillStyle = 'red'
+        this.ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw();
+
+        // this.position.x += this.velocity.x
+        if (this.position.x + this.velocity.x >= 0 && this.position.x + this.velocity.x + this.width <= this.canvas.width) {
+            this.position.x += this.velocity.x;
+          }
+        
+
+        this.position.y += this.velocity.y
+        if (this.position.y + this.height + this.velocity.y < this.canvas.height)
+            {this.velocity.y += CONSTANTS.gravity}
+        else 
+            {this.velocity.y = 0; this.isJumping = false}
+    }
+
+    jump() {
+        if (!this.isJumping) {
+            this.velocity.y = -10
+            this.isJumping = true
+        }
+    }
+}
+
+export default Player;
